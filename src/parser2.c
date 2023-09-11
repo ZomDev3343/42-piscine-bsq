37f60a1ea5be94a42483509f28ed5609983db397
Thomas Ruello <truello@c2r6p3.42perpignan.fr>
2023-09-11 18:03:14 +0200

    Verification de l uniformite des longueurs des lignes

--- src/parser2.c
+++ src/parser2.c
@@ -6,7 +6,7 @@
 /*   By: truello <thomasdelan2@gmail.com>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/11 17:11:55 by truello           #+#    #+#             */
-/*   Updated: 2023/09/11 17:38:28 by truello          ###   ########.fr       */
+/*   Updated: 2023/09/11 18:01:49 by truello          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,7 @@ int	validate_grid(t_char *grid)
 	{
 		if (ft_strlen_c(&grid[i], '\n') != line_len)
 			return (0);
-		i += line_len + 1;
+		i += line_len;
 	}
 	return (line_len);
 }
